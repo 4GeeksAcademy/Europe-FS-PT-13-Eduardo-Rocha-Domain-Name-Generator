d7b48d2c04c5d9af63c3a5687517117d08225886
Eduardo Rocha <134506270+eddyrocha78@users.noreply.github.com>
2023-08-11 20:00:18 +0000

    trying to print in html

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  funtion;
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
