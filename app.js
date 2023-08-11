61c18803158ed56084080c2a2de81357dbb51855
Eduardo Rocha <134506270+eddyrocha78@users.noreply.github.com>
2023-08-11 19:04:51 +0000

    added ;

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ let adj = ['great', 'big' ];
 let noun = ['jogger','racoon'];
 let domain = ['.com', '.net','.io']
 
-for (let i = 0, i < pronoun.length, i++) {
-    for (let j = 0, j < adj.length, j++) {
-        for (let k = 0, k < noun.length, k++) {
-            for (let l = 0, l < domain.length, l++) {
+for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+            for (let l = 0; l < domain.length; l++) {
             }
         }
     }
