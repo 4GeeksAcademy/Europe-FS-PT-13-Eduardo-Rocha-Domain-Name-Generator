9c48929553f633c7038977db5699a4e6dc04adda
Eduardo Rocha <134506270+eddyrocha78@users.noreply.github.com>
2023-08-11 19:03:08 +0000

    p index rectified

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ window.onload = function() {
   let noun = ["jogger", "racoon"];
   let domain = [".com", ".net", ".io"];
 
-  for (let i = 0; i < pronoun.length; p++) {
+  for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
         for (let l = 0; l < domain.length; l++) {
